c8e99cb44ce3895c2b93907a55f41de3086e843c
Blyss Sarania <blyss.sarania@gmail.com>
2024-03-27 18:24:56 -0400

    Add missing download_model.sh

---
+++ download_model.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source ./venv/bin/activate
+
+# Run the Python script
+python download_model.py
+
+# Deactivate the virtual environment
+deactivate
